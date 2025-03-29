675f7b895c34aec3272e7c62e599420ed64e14ae
Talha Tufan <talha.tufan@hotmail.com>
2025-03-29 12:49:31 +0300

    Trend filmler kısmı güncellendi

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,58 +1,71 @@
-import { View, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image } from 'react-native'
-import React from 'react'
-import { styles } from '../theme/theme'
-import { useNavigation } from '@react-navigation/native';
-import { Dimensions } from 'react-native';
-import { fallbackMoviePoster, image185 } from '../api/moviedb';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+} from "react-native";
+import React from "react";
+import { styles } from "../theme/theme";
+import { useNavigation } from "@react-navigation/native";
+import { Dimensions } from "react-native";
+import { fallbackMoviePoster, image185 } from "../api/moviedb";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
-export default function MovieList({title, data, hideSeeAll}) {
+export default function MovieList({ title, data, hideSeeAll }) {
   const navigation = useNavigation();
-  let movieName="The Green Mile"
+  let movieName = "The Green Mile";
   return (
     <View className="mb-8 space-y-4">
       <View className="flex-row justify-between items-center bg-neutral-900 py-2 border-b-2 border-t-2 border-neutral-800">
         <Text className="text-white text-xl font-black mx-5 ">{title}</Text>
-        {  
-          !hideSeeAll &&(
-            <TouchableOpacity>
-              <Text style={styles.text} className="mx-2">Tümünü Gör</Text> 
-            </TouchableOpacity>
-          )  
-      } 
+        {!hideSeeAll && (
+          <TouchableOpacity>
+            <Text style={styles.text} className="mx-2">
+              Tümünü Gör
+            </Text>
+          </TouchableOpacity>
+        )}
       </View>
-       {/* Film Satırı */}
-        <ScrollView 
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{paddingHorizontal: 15}}>
-          {
-            data.map((item,index)=>{
-              return(
-                <TouchableWithoutFeedback key={index} onPress={()=>navigation.push('Movie',item)}
-                >
-                  <View className="space-y-1 mr-4">
-                      <Image
-                        source={{uri:image185(item.poster_path) || fallbackMoviePoster }}
-                        // source={require('../assets/image3.jpg')}
-                        className="rounded-3xl"
-                        style={{ width: windowWidth*0.3, height: windowHeight*0.22, borderRadius: 20 }}/>
-                    
-                    <Text className="text-neutral-300 ml-2">
-                    {
-                    (item?.title || "").length > 15 
-                    ? (item?.title || "").slice(0, 15) + "..." 
-                    : (item?.title || "")
-                    }</Text>
-                  </View>
-                </TouchableWithoutFeedback>
-                )
-              }
-            )
-          }
-        </ScrollView>
-      </View>
-  )
+      {/* Film Satırı */}
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+      >
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => navigation.push("Movie", item)}
+            >
+              <View className="space-y-1 mr-4 items-center">
+                <Image
+                  source={{
+                    uri: image185(item.poster_path) || fallbackMoviePoster,
+                  }}
+                  // source={require('../assets/image3.jpg')}
+                  className="rounded-3xl"
+                  style={{
+                    width: windowWidth * 0.3,
+                    height: windowHeight * 0.22,
+                    borderRadius: 20,
+                  }}
+                />
+
+                <Text className="text-neutral-300">
+                  {(item?.title || "").length > 15
+                    ? (item?.title || "").slice(0, 15) + "..."
+                    : item?.title || ""}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
 }
--- components/TrendingMovies.js
+++ components/TrendingMovies.js
@@ -1,61 +1,81 @@
-import React from 'react'
-import { View, Text, TouchableWithoutFeedback, Dimensions, Image } from 'react-native'
-import Carousel from 'react-native-reanimated-carousel'
-import Animated from 'react-native-reanimated'
-import { useNavigation } from '@react-navigation/native'
-import { Platform } from 'react-native'
-import { image500 } from '../api/moviedb'
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Dimensions,
+  Image,
+} from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+import { useNavigation } from "@react-navigation/native";
+import { Platform } from "react-native";
+import { image500 } from "../api/moviedb";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-const ios=Platform.OS=="ios"
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const ios = Platform.OS == "ios";
 
-export default function TrendingMovies({data}) {
-  const navigation = useNavigation()
+export default function TrendingMovies({ data }) {
+  const navigation = useNavigation();
   const handleClick = (item) => {
-    navigation.navigate('Movie',item)
-  }
+    navigation.navigate("Movie", item);
+  };
+
   return (
     <View justifyContent="center">
-      <View className="bg-neutral-800"> 
-        <Text className="text-white text-xl p-1 font-black mx-4">Trend Filmler</Text>
+      <View className="bg-neutral-800 border border-l-2 border-r-2 border-y-neutral-900 border-x-cyan-50 ">
+        <Text className="text-cyan-50 text-xl p-1 font-black self-center">
+          Trend Filmler
+        </Text>
       </View>
-      
-        <View dataSet={{ kind: "basic-layouts", name: "parallax" }} id="carousel-component">
-        
-          <Carousel
-            loop={false}
-            width={windowWidth}
-            mode="parallax"
-            modeConfig={{
-              parallaxScrollingOffset: 0.9,
-              parallaxScrollingScale: 0.9,
-            }}
-            height={windowHeight * 0.4}
-            data={data}
-            scrollAnimationDuration={1000}
-            renderItem={({item}) => <MovieCard item={item} handleClick={handleClick}/>}
-            autoPlay={false}
-            // autoPlayInterval={1000}
-            pagingEnabled={true}
-            snapToAlignment="center"
-          />
+
+      <View
+        dataSet={{ kind: "basic-layouts", name: "parallax" }}
+        id="carousel-component"
+      >
+        <Carousel
+          loop={false}
+          width={windowWidth}
+          mode="parallax"
+          modeConfig={{
+            parallaxScrollingOffset: 170, // Öğeler arası yakınlık
+            parallaxScrollingScale: 0.9, // Aktif öğenin boyutu
+            parallaxAdjacentItemScale: 0.7, // Yanındaki öğenin boyutu
+          }}
+          height={windowHeight * 0.4}
+          data={data}
+          scrollAnimationDuration={1000}
+          renderItem={({ item }) => (
+            <MovieCard item={item} handleClick={handleClick} />
+          )}
+          autoPlay={false}
+          // autoPlayInterval={1000}
+          pagingEnabled={true}
+          snapEnabled={true}
+          snapToAlignment="center"
+          style={{
+            alignSelf: "center",
+            width: windowWidth,
+          }}
+        />
       </View>
     </View>
-    
-  )
+  );
 }
 
-const MovieCard = ({item,handleClick}) => {
-    // console.log('item.poster_path',item.poster_path);
-    return (
-        <TouchableWithoutFeedback onPress={()=>handleClick(item)} >
-          <Image 
-            // source={require('../assets/image2.jpg')}
-            source={{uri:image500(item.poster_path)}}
-            style={{ width: windowWidth*0.6, height: windowHeight * 0.4, borderRadius: windowWidth*0.06 }}
-            resizeMode="cover"
-          />
-        </TouchableWithoutFeedback>
-    )
-}
+const MovieCard = ({ item, handleClick }) => {
+  return (
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+      <Image
+        source={{ uri: image500(item.poster_path) }}
+        style={{
+          width: windowWidth * 0.65, // Film posterinin genişliği
+          height: windowHeight * 0.4,
+          borderRadius: windowWidth * 0.06,
+        }}
+        resizeMode="cover"
+        className="self-center"
+      />
+    </TouchableWithoutFeedback>
+  );
+};
